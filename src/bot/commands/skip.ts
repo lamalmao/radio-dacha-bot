d5b982c61bbf633e1aec32e33e626f665741585d
lamalmao <114337080+lamalmao@users.noreply.github.com>
2023-08-02 19:12:26 +0300

    изменен skip

--- src/bot/commands/skip.ts
+++ src/bot/commands/skip.ts
@@ -1,7 +1,6 @@
 import { Message } from 'discord.js';
 import Command from './command';
 import { channels } from '..';
-import { AudioPlayerStatus } from '@discordjs/voice';
 
 export class SkipMusic implements Command {
   async execute(message: Message) {
@@ -11,11 +10,7 @@ export class SkipMusic implements Command {
         return;
       }
 
-      if (channelData.player.state.status === AudioPlayerStatus.Playing) {
-        channelData.player.emit('next');
-      } else {
-        message.reply('Сейчас ничего не играет');
-      }
+      channelData.player.emit('next');
     } catch (error) {
       message.reply((error as Error).message);
     }
